ebfc59273edacb195a3943b4776999dff08a06a4
Ashley Woodford <ashley@coldbananastudio.com>
2021-04-19 14:20:46 +0100

    rm Netlify CMS integration

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,15 +2,6 @@ import glob from 'glob'
 import path from 'path'
 import * as SITE_INFO from './assets/content/site/info.json'
 
-const dynamicContentPath = 'assets/content'
-const dynamicRoutes = getDynamicPaths(
-  {
-    blog: 'blog/*.json',
-    casestudies: 'case-studies/*.json'
-  },
-  dynamicContentPath
-)
-
 export default {
   env: {
     url:
@@ -39,11 +30,6 @@ export default {
         href: 'https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,400;0,700;1,400&display=swap'
       }
     ]
-  },
-  generate: {
-    routes: dynamicRoutes,
-    fallback: true,
-    subFolders: false
   },
   /*
    ** Customize the progress-bar color
@@ -78,32 +64,4 @@ export default {
      */
     extend(config, ctx) {}
   }
-}
-
-/**
- * Create an array of URLs from a list of files
- * @param {*} urlFilepathTable - example below
- * {
- *   blog: 'blog/*.json',
- *   projects: 'projects/*.json'
- * }
- *
- * @return {Array} - Will return those files into urls for SSR generated .html's like
- * [
- *   /blog/2019-08-27-incidunt-laborum-e ,
- *   /projects/story-test-story-1
- * ]
- */
-function getDynamicPaths(urlFilepathTable, cwdPath) {
-  console.log('Going to generate dynamicRoutes for these collection types: ', urlFilepathTable)
-  const dynamicPaths = [].concat(
-    ...Object.keys(urlFilepathTable).map(url => {
-      const filepathGlob = urlFilepathTable[url]
-      return glob.sync(filepathGlob, { cwd: cwdPath }).map(filepath => {
-        return `/${url}/${path.basename(filepath, '.json')}`
-      })
-    })
-  )
-  console.log('Found these dynamicPaths that will be SSR generated:', dynamicPaths)
-  return dynamicPaths
 }
